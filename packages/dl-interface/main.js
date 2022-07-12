74011b0b61fba33965f92e57989d22cb105e4847
liumingedwin <liumingedwin@163.com>
2022-07-12 21:33:22 +0800

    FIX (1/2): ENABLE LAZY-LOADING & FIX SOME OF Tree Bug

--- packages/dl-interface/main.js
+++ packages/dl-interface/main.js
@@ -1,39 +1,34 @@
-(function() {
-    if (1) {
+(function () {
+    if (0) {
         var tree = [{
-                text: "Parent 1",
-                collapseIcon: true,
-                showCheckbox: true,
-                expandIcon: "fa fa-plus",
-                selectable: true,
+            text: "Parent 1",
+            nodes: [{
+                text: "Child 1",
                 nodes: [{
-                        text: "Child 1",
-                        collapseIcon: true,
-                        nodes: [{
-                                text: "Grandchild 1"
-                            },
-                            {
-                                text: "Grandchild 2"
-                            }
-                        ]
-                    },
-                    {
-                        text: "Child 2"
-                    }
+                    text: "Grandchild 1"
+                },
+                {
+                    text: "Grandchild 2"
+                }
                 ]
             },
             {
-                text: "Parent 2"
-            },
-            {
-                text: "Parent 3"
-            },
-            {
-                text: "Parent 4"
-            },
-            {
-                text: "Parent 5"
+                text: "Child 2"
             }
+            ]
+        },
+        {
+            text: "Parent 2"
+        },
+        {
+            text: "Parent 3"
+        },
+        {
+            text: "Parent 4"
+        },
+        {
+            text: "Parent 5"
+        }
         ];
         $('#download-tree').treeview({ data: tree });
         tree[0].nodes.push({
@@ -43,26 +38,44 @@
     }
     let download_tree = [{
         text: "version.json",
-        nodes: []
+        nodes: [{
+            text: "version.assets.json"
+        }]
     }];
 
+    function reflush_tree() {
+        $('#download-tree').treeview({ data: download_tree, showCheckbox: true, expandIcon: "fa fa-plus", collapseIcon: "fa fa-minus" });
+    }
+    reflush_tree();
+    let last_refresh = new Date();
     function create_branch(branch) {
-        let tree = download_tree;
-        for (let i = 0; i < branch.length - 1; ++i) {
-            let place=-1;
-            for (let j=0;j<tree.nodes.length;++j){
-                if(tree.nodes[j].text==branch[i]){
-                    place=j;
+        let tree = { nodes: download_tree };
+        for (let i = 0; i < branch.length; ++i) {
+            console.log(branch[i], i, branch.length)
+            let place = -1;
+            for (let j = 0; j < tree.nodes.length; ++j) {
+                if (tree.nodes[j].text == branch[i]) {
+                    place = j;
                     break;
                 }
             }
             if (place == -1) {
+                console.log(i + 1,branch.length)
                 tree.nodes.push({
-                    text:branch[i],
-                    nodes:[]
+                    text: branch[i],
+                    nodes: [],
+                    expandIcon: (i + 1 >= branch.length) ? ("null-icon") : (undefined),
+                    collapseIcon: (i + 1 >= branch.length) ? ("null-icon") : (undefined),
                 });
+                tree = tree.nodes[tree.nodes.length - 1];
+            } else {
+                tree.expandIcon = undefined;
+                tree.collapseIcon = undefined;
+                tree = tree.nodes[place];
             }
         }
+        if (new Date() - last_refresh > 1000)
+            reflush_tree(), last_refresh = new Date();
     }
     window.worker = new Worker("../resourcer/index.js");
     worker.postMessage(JSON.stringify({
@@ -72,17 +85,29 @@
         mirror: localStorage["api"],
         natives: JSON.parse(sessionStorage["data-natives"])
     }));
-    worker.onmessage = function(e) {
-        console.log(e.data);
+    worker.onmessage = function (e) {
         let data = e.data;
-        switch (data.type) {
+        console.log(data)
+        let message;
+        try {
+            message = JSON.parse(data);
+        } catch (e) {
+            console.warn("BAD PACKET.DEPREDATED.", e);
+        }
+
+
+
+        switch (message.type) {
             case "add-file":
                 {
-                    let branch = data["file-path"].split("/");
+                    let branch = message["tree"];
                     create_branch(branch);
                     break;
                 }
         }
+    }
+    window.installer = {
+        download_tree: download_tree
     }
 })();
 /*
